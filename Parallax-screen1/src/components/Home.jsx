1850e3a6279056af7fbe10cc8b8bc69c22b568e5
sandeepbeherakbl <sandeepbeherakbl@gmail.com>
2024-05-15 12:21:37 +0530

    parallax screen about section added

--- Parallax-screen1/src/components/Home.jsx
+++ Parallax-screen1/src/components/Home.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import Swiper from 'swiper/bundle';
-import 'swiper/css';
-import { gsap, Power4 } from 'gsap';
-import Parallax from 'parallax-js';
-import './Home.css';
-import Background from "./images/background.png"
+import { useEffect } from "react";
+import Swiper from "swiper/bundle";
+import "swiper/css";
+import { gsap, Power4 } from "gsap";
+import Parallax from "parallax-js";
+import "./Home.css";
+import Background from "./images/background.png";
 import Rock from "./images/rock.png";
 import Earth from "./images/earth.png";
 import Mid from "./images/mid.png";
@@ -13,155 +13,162 @@ import linkedin from "./images/linkedin.png";
 import facebook from "./images/facebook.png";
 import Instagram from "./images/instagram-logo.png";
 import Logo from "./images/my-logo.png";
-import Saturn from "./images/saturn.png";
-
-
+import ScrollDown from "./images/Animation.gif";
+import React from "./images/React.png";
 
 const Planets = () => {
   useEffect(() => {
-    const scene = document.getElementById('scene');
+    const scene = document.getElementById("scene");
     const parallaxInstance = new Parallax(scene);
 
     const keys = [
-      'Mercury',
-      'Venus',
-      'Earth',
-      'Mars',
-      'Jupiter',
-      'Saturn',
-      'Uranus',
-      'Neptune'
+      "Mercury",
+      "Venus",
+      "Earth",
+      "Mars",
+      "Jupiter",
+      "Saturn",
+      "Uranus",
+      "Neptune",
     ];
 
-    const slider = new Swiper('.swiper-container', {
-      slidesPerView: 'auto',
+    const slider = new Swiper(".swiper-container", {
+      slidesPerView: "auto",
       spaceBetween: 150,
       centeredSlides: true,
       mousewheel: true,
       pagination: {
-        el: '.planet-links',
+        el: ".planet-links",
         clickable: true,
-        renderBullet: function(index, className) {
-          return '<div class="' + className + '">' + keys[index] + '</div>';
-        }
-      }
+        renderBullet: function (index, className) {
+          return '<div class="' + className + '">' + keys[index] + "</div>";
+        },
+      },
     });
 
-    slider.on('slideChange', () => {
-      console.log('SLIDE CHANGED');
-      gsap.to('.slide-text span', 0.2, {
-        x: '-100px'
+    slider.on("slideChange", () => {
+      console.log("SLIDE CHANGED");
+      gsap.to(".slide-text span", 0.2, {
+        x: "-100px",
       });
-      gsap.to('.slide-number span', 0.2, {
-        x: '-100px'
+      gsap.to(".slide-number span", 0.2, {
+        x: "-100px",
       });
-      gsap.to('.slide-detail span', 0.5, {
-        x: '-1000px'
+      gsap.to(".slide-detail span", 0.5, {
+        x: "-1000px",
       });
-      gsap.to('.slide-detail-facts div', 0.5, {
-        x: '-1000px'
+      gsap.to(".slide-detail-facts div", 0.5, {
+        x: "-1000px",
       });
-      gsap.to('.swiper-slide-active', 0.5, {
-        scale: 0.85
+      gsap.to(".swiper-slide-active", 0.5, {
+        scale: 0.85,
       });
-      gsap.to('.swiper-slide .slide-img', 1, {
-        rotation: 20
+      gsap.to(".swiper-slide .slide-img", 1, {
+        rotation: 20,
       });
     });
 
-    slider.on('slideChangeTransitionEnd', () => {
-      gsap.to('.swiper-slide .slide-img', 1, {
-        rotation: 10
+    slider.on("slideChangeTransitionEnd", () => {
+      gsap.to(".swiper-slide .slide-img", 1, {
+        rotation: 10,
       });
-      gsap.to('.slide-text span', 0.2, {
+      gsap.to(".slide-text span", 0.2, {
         x: 0,
-        delay: 0.1
+        delay: 0.1,
       });
-      gsap.to('.slide-text span', 0, {
-        x: '100px'
-      });
-
-      gsap.to('.slide-number span', 0.2, {
-        x: 0
-      });
-      gsap.to('.slide-number span', 0, {
-        x: '100px'
+      gsap.to(".slide-text span", 0, {
+        x: "100px",
       });
 
-      gsap.to('.slide-detail span', 0.2, {
-        x: 0
+      gsap.to(".slide-number span", 0.2, {
+        x: 0,
+      });
+      gsap.to(".slide-number span", 0, {
+        x: "100px",
       });
 
-      gsap.to('.slide-detail-facts div', 0.5, {
-        x: 0
+      gsap.to(".slide-detail span", 0.2, {
+        x: 0,
       });
 
-      gsap.to('.swiper-slide-active', 0.5, {
+      gsap.to(".slide-detail-facts div", 0.5, {
+        x: 0,
+      });
+
+      gsap.to(".swiper-slide-active", 0.5, {
         scale: 1,
-        ease: Power4.easeOut
+        ease: Power4.easeOut,
       });
 
-      gsap.to('.swiper-slide-active .slide-text', 0, {
-        autoAlpha: 1
+      gsap.to(".swiper-slide-active .slide-text", 0, {
+        autoAlpha: 1,
       });
-      gsap.to('.swiper-slide-active .slide-number', 0, {
-        autoAlpha: 1
-      });
-
-      gsap.to('.swiper-slide-next .slide-text', 0, {
-        autoAlpha: 0
-      });
-      gsap.to('.swiper-slide-prev .slide-text', 0, {
-        autoAlpha: 0
+      gsap.to(".swiper-slide-active .slide-number", 0, {
+        autoAlpha: 1,
       });
 
-      gsap.to('.swiper-slide-next .slide-number', 0, {
-        autoAlpha: 0
+      gsap.to(".swiper-slide-next .slide-text", 0, {
+        autoAlpha: 0,
       });
-      gsap.to('.swiper-slide-prev .slide-number', 0, {
-        autoAlpha: 0
+      gsap.to(".swiper-slide-prev .slide-text", 0, {
+        autoAlpha: 0,
+      });
+
+      gsap.to(".swiper-slide-next .slide-number", 0, {
+        autoAlpha: 0,
+      });
+      gsap.to(".swiper-slide-prev .slide-number", 0, {
+        autoAlpha: 0,
       });
     });
 
-    gsap.to('.rockbg1', 2, {
+    gsap.to(".rockbg1", 2, {
       y: 12,
       repeat: -1,
       yoyo: true,
-      delay: 0
+      delay: 0,
     });
 
-    gsap.to('.swiper-slide-next .slide-text', 0, {
-      autoAlpha: 0
+    gsap.to(".swiper-slide-next .slide-text", 0, {
+      autoAlpha: 0,
     });
-    gsap.to('.swiper-slide-prev .slide-text', 0, {
-      autoAlpha: 0
-    });
-
-    gsap.to('.swiper-slide-next .slide-number', 0, {
-      autoAlpha: 0
-    });
-    gsap.to('.swiper-slide-prev .slide-number', 0, {
-      autoAlpha: 0
+    gsap.to(".swiper-slide-prev .slide-text", 0, {
+      autoAlpha: 0,
     });
 
-    gsap.to('.swiper-slide', 0, {
-      scale: 0.85
+    gsap.to(".swiper-slide-next .slide-number", 0, {
+      autoAlpha: 0,
+    });
+    gsap.to(".swiper-slide-prev .slide-number", 0, {
+      autoAlpha: 0,
     });
 
-    gsap.to('.swiper-slide-active', 0, {
-      scale: 1
+    gsap.to(".swiper-slide", 0, {
+      scale: 0.85,
+    });
+
+    gsap.to(".swiper-slide-active", 0, {
+      scale: 1,
     });
   }, []);
 
   return (
     <div>
       <nav>
-        <div className="logo"><img src={Logo} alt="LinkedIn" /></div>
+        <div className="logo">
+          <img src={Logo} alt="LinkedIn" />
+        </div>
         <div className="nav-links">
           <ul>
-            <li><img src={linkedin} alt="LinkedIn" /></li>
-            <li><img src={facebook} alt="Facebook" /></li>
-            <li><img src={Instagram} alt="Instagram" /></li>
+            <li>
+              <img src={linkedin} alt="LinkedIn" />
+            </li>
+            <li>
+              <img src={facebook} alt="Facebook" />
+            </li>
+            <li>
+              <img src={Instagram} alt="Instagram" />
+            </li>
           </ul>
         </div>
       </nav>
@@ -188,10 +195,43 @@ const Planets = () => {
             <img src={forground} alt="Foreground" />
           </div>
         </div>
+        <div className="scrollDown">
+          <img src={ScrollDown} alt="" />
+        </div>
       </section>
 
-      <div style={{height: "100vh"}}>
+      {/* about section */}
+      <div style={{ height: "100vh" }}>
+        <div className="about-container container-background">
+          <div className="container-text">
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad
+              nesciunt quas natus nostrum molestiae suscipit ab earum quisquam
+              dolorem quam.
+            </p>
+          </div>
+          <div className="container-image-div"></div>
+        </div>
+      </div>
 
+      {/* skills section */}
+      <div style={{ height: "100vh" }}>
+        <div className="skills-container container-background">
+          <div className="container-text">
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad
+              nesciunt quas natus nostrum molestiae suscipit ab earum quisquam
+              dolorem quam.
+            </p>
+          </div>
+          <div className="container-image-div">
+            <div className="skills-div">
+              <img src={React} alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
